807ee5ca9d02b9407e25d0e28fad3210d2767b47
Mário Santos <marionbsantos@sapo.pt>
2014-02-27 22:38:03 +0000

    Frontend with 960gs 12 columns

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ define('mariosir',true);
 ?>
 <html>
 <head>
-	<link rel="stylesheet" href="./assets/style.css" type="text/css">
+	<link rel="stylesheet" href="./assets/theme.css" type="text/css">
 	<title>Que filme ver?</title>
 	<meta name="description" content="Sem ideias para filmes? N&oacute;s temos algumas sugest&otilde;es!">
 	<meta property="og:image" content="http://www.quefilme.com/assets/logo.png"/>
@@ -14,36 +14,28 @@ define('mariosir',true);
 	<link rel="icon" type="image/ico" href="./assets/favicon.ico"/>
 </head>
 <body>
-	<div id="main">
-		<h1><a href="/">Que filme ver?</a></h1>
-		<div id="poster">
-			<a href="#" rel="nofollow" target="_blank"><img src="http://placehold.it/200x282" width=200 height=282></a>
+	<div class="container_12" id="main">
+		<div class="grid_2">
+			<img width="140" height="140" src="./assets/logo.png" />
 		</div>
-		<div id="header">
-			<div id="hcontent">
-				<p id="reason">
-					Sem ideias para filmes? Temos aqui umas sugest&otilde;es!
-				</p>
-				<h2>Em construção...</h2>
-				<p id="meta">
-					
-				</p>
+		<div class="grid_8">
+			<div class="filme">
+				<p class="lead">Sem ideias para filmes? Temos aqui umas sugest&otilde;es!</p>
+				<h2>Nebraska</h2>
+				<p>Aventura / Drama <br />115 minutos <br />Bruce Dern, Will Forte, June Squibb, Bob Odenkirk</p>
+			</div>
+			<div class="player">
+				<iframe width="620" height="349" src="//www.youtube.com/embed/9_MTRKedJ5U" frameborder="0" allowfullscreen></iframe>
 			</div>
 		</div>
-		<div id="player">
-			<iframe width="475" height="288" src="//www.youtube.com/embed/OYlBx3TDEKs" frameborder="0" allowfullscreen></iframe>
-		</div>
-		<div id="sayhi">
-			Desenvolvido por M&aacute;rio Santos
+		<div class="grid_2">
+			<div class="poster">
+				<a href="#" rel="nofollow" target="_blank"><img src="http://placehold.it/140x197" /></a>
+			</div>
 		</div>
 	</div>
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-  ga('create', 'UA-41978760-1', 'quefilme.com');
-  ga('send', 'pageview');
-</script>
+	<div class="footer">
+		Desenvolvido por M&aacute;rio Santos
+	</div>
 </body>
 </html>
